5487281245bd8102669a4129e7c18b8b2601c05a
kvanhecke <kris0476@gmail.com>
2011-05-10 12:15:39 +0200

    remove deprecated Controller class

--- Test.php
+++ Test.php
@@ -32,13 +32,9 @@ require_once dirname(__FILE__) . '/SugestioUser.php';
 require_once dirname(__FILE__) . '/SugestioItem.php';
 require_once dirname(__FILE__) . '/SugestioConsumption.php';
 
-require_once dirname(__FILE__) . '/Controller.php';
 
 // test the sugestio client object
 $test = new SugestioClientTest();
-
-// test the controller object that provides backwards compatibility with the old library
-//$test = new ControllerTest();
 
 try {
 
@@ -193,65 +189,6 @@ class SugestioClientTest {
 		print_r($result);
 		echo '</pre>';
 	}
-}
-
-/**
- * This class will test the Controller.php wrapper functions.
- */
-class ControllerTest {
-
-	private $controller;
-
-	public function __construct() {
-		$this->controller = new Controller();
-	}
-
-	public function testAddUser() {
-		$result = $this->controller->addUser(1, 'NY', '40.446195,-79.948862', 'M', '1975-04-09');
-		echo "addUser response code: $result";
-	}
-
-	public function testAddItem() {
-
-		$result = $this->controller->addItem(1, '2010-01-01T00:00:00', '2010-12-31T00:00:00',
-				'vooruit', '40.446195,-79.948862',
-				array('artist 1', 'artist 2'), array('tag 1', 'tag 2'), array('category 1', 'category 2'));
-
-		echo "addItem response code: $result";
-	}
-
-	public function testAddConsumption() {
-		
-		$extra = array('phrase' => 'recommended for you', 'algorithm' => 'UserBasedCF');
-		$result = $this->controller->addConsumptionExtra('1', 'abcdef', 'RATING', 'THUMB:UP', 'NOW', 'home', '40.446195,-79.948862', $extra);
-		echo "addConsumption response code: $result";
-	}
-
-	public function testGetRecommendations() {
-
-		//$recommendations = $this->client->getRecommendations(1, array('category' => 'music'));
-		//$recommendations = $this->client->getRecommendations(1, array('segment' => 'en-US'));
-		$recommendations = $this->controller->getRecommendations(1);
-
-		echo '<pre>';
-		print_r($recommendations);
-		echo '</pre>';
-	}
-
-	public function testGetSimilar() {
-
-		$similar = $this->controller->getSimilar(11256);
-
-		echo '<pre>';
-		print_r($similar);
-		echo '</pre>';
-	}
-
-	public function testDeleteRecommendation() {
-		$result = $this->controller->deleteRecommendation(1, 'abcd');
-		echo "deleteRecommendation response code: $result";
-	}
-
 }
 
 ?>
